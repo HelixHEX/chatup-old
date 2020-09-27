e402ad94478267d7cf625544416037d54a910b1f
HelixHEX <heyy.im.eli@gmail.com>
2020-09-26 23:23:57 -0400

    :fire: Real-time implementation for notifications

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,96 +1,107 @@
 //config stuff
 import { __prod__, COOKIE_NAME } from "./constants";
-import path from 'path';
-import 'dotenv-safe/config';
-import 'reflect-metadata';
+import path from "path";
+import "dotenv-safe/config";
+import "reflect-metadata";
 
 //express
 import express from "express";
 
 //cors
-import cors from 'cors'
+import cors from "cors";
 
 //graphql
-import { ApolloServer } from "apollo-server-express";
+import { ApolloServer, PubSub } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
+// import { RedisPubSub } from "graphql-redis-subscriptions";
+// import { subscribe, execute } from "graphql";
+// import { WebSocketLink } from "apollo-link-ws";
+// import { SubscriptionClient } from "subscriptions-transport-ws";
 
 //typeorm
-import { createConnection } from 'typeorm';
+import { createConnection } from "typeorm";
 
 //entities
-import { User } from './entities/User'
-import { Message } from './entities/Message'
-import { Chatroom } from './entities/Chatroom'
+import { User } from "./entities/User";
+import { Message } from "./entities/Message";
+import { Chatroom } from "./entities/Chatroom";
 
 //resolvers
-import { UserResolver } from './resolvers/user'
-import { ChatroomResolver } from './resolvers/chatroom'
-
+import { UserResolver } from "./resolvers/user";
+import { ChatroomResolver } from "./resolvers/chatroom";
 
 //redis/sessions
 import redis from "redis";
 import session from "express-session";
 import connectRedis from "connect-redis";
+// import { SubscriptionServer } from "subscriptions-transport-ws";
+import { createServer } from "http";
 
 const RedisStore = connectRedis(session);
-  const redisClient = redis.createClient();
+const redisClient = redis.createClient();
 
 const main = async () => {
   //typeorm setup
   await createConnection({
-    type: 'postgres',
+    type: "postgres",
     url: process.env.DATABASE_URL,
     logging: true,
     synchronize: true,
-    migrations: [path.join(__dirname, './migrations/*')],
-    entities: [User, Message, Chatroom]
+    migrations: [path.join(__dirname, "./migrations/*")],
+    entities: [User, Message, Chatroom],
   });
 
   //express setup
-  const app = express()
+  const app = express();
 
   //setup cors
   app.use(
     cors({
       origin: process.env.CORS_ORIGIN,
-      credentials: true
+      credentials: true,
     })
-  )
+  );
 
   //setup redis/sessions
   app.use(
     session({
       name: COOKIE_NAME,
-      store: new RedisStore({ 
+      store: new RedisStore({
         client: redisClient,
         disableTouch: true,
       }),
       cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
         httpOnly: true,
-        sameSite: 'lax',
-        secure: __prod__
+        sameSite: "lax",
+        secure: __prod__,
       },
       saveUninitialized: false,
       secret: process.env.SESSION_SECRET,
-      resave: false
+      resave: false,
     })
-  )
+  );
 
   //setup apollo server
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [UserResolver, ChatroomResolver],
       validate: false,
     }),
-    context: ({req, res}) => ({ req, res }),
+    context: ({ req, res }) => ({ req, res }),
   });
 
   apolloServer.applyMiddleware({ app, cors: false });
 
-  app.listen(process.env.PORT, () => {
-    console.log("server started on localhost:5000");
-  });
-}
+  //subscriptions
+  const subscriptionsServer = createServer(app);
+  apolloServer.installSubscriptionHandlers(subscriptionsServer);
 
-main()
+  subscriptionsServer.listen(process.env.PORT, () => {
+    console.log(`🚀 Server ready at http://localhost:5000${apolloServer.graphqlPath}`);
+    console.log(`🚀 Subscriptions ready at ws://localhost:5000${apolloServer.subscriptionsPath}`);
+  });
+};
+
+main();
